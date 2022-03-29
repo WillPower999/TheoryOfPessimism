b1cce9f73c6285ce593d29f02a30023c495e2499
WillPower999 <williamcordelllegensworm@gmail.com>
2022-03-29 13:59:40 -0500

    titlescreen

---
+++ TheoryOfPessimism/Assets/Scripts/TitleScroll.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScroll : MonoBehaviour
+{
+    public Vector3 translation;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(translation * Time.deltaTime);
+    }
+}
